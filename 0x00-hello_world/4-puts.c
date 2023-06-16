321747ef918f237da50a015a765bd362326103e2
hepishy <hepishy@gmail.com>
2023-06-15 19:00:05 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C program that print string
+ * Return: Return 0 if success 1 otherwise
+*/
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
