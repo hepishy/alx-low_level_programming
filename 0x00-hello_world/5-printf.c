740fc0e8f1e3c51ee5245a1e394d278fe5c01c31
hepishy <hepishy@gmail.com>
2023-06-15 19:08:09 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: C program that print string
@@ -6,6 +7,6 @@
 */
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
