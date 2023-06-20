4f6875d6adaf6363685f3c1d4850d26acee8a144
hepishy <hepishy@gmail.com>
2023-06-20 14:17:37 -0700

    task 1 and 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _putchar - writes character c to stdout
+ * print_alphabet - writes character c to stdout
  * Description: pring alphabet lowercase
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
