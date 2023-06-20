096055d1e119120b7bea7b194391ca04e3621a25
hepishy <hepishy@gmail.com>
2023-06-20 14:13:32 -0700

    task 1 and 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * _putchar - writes character c to stdout
- *
+ * Description: pring alphabet lowercase
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
