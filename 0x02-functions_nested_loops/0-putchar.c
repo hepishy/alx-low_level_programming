26d1e0397afecf8d35aa2adce878a7570afb246b
hepishy <hepishy@gmail.com>
2023-06-20 13:43:27 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,6 +7,13 @@
 */
 int main(void)
 {
-	printf("_putchar\n");
+
+	char text[]="_putchar\n";
+	int i = 0;
+	while (text[i] != '\0')
+	{
+		_putchar(text[i]);
+		i++;
+	}
 	return (0);
 }
