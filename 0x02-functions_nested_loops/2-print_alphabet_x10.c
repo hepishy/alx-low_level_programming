1ac8ba40c465fb719d83b172dedb79f0fa315f6f
hepishy <hepishy@gmail.com>
2023-06-20 14:05:36 -0700

    task 1 and 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - writes character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return;
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c~
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _putchar - writes character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return;
+}
