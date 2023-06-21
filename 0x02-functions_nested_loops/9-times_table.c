67a8ad56fe1cb4fbe0a9c59df8ee15d0cb940f51
hepishy <hepishy@gmail.com>
2023-06-20 19:36:35 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table 9
+ *
+ * Return: void.
+ */
+void times_table(void)
+{
+	int i, j;
+	int div, mod, prod;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			prod = i * j;
+			div = prod / 10;
+			mod = prod % 10;
+			if (prod > 9)
+				_putchar('0' + div);
+			else
+				_putchar(' ');
+			_putchar('0' + mod);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			}
+		_putchar('\n');
+	}
+}
